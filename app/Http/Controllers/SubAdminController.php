c97c9d78cac709a89562f1c35e0758801065bb98
suresh sarkar <sureshsarkar2020@gmail.com>
2024-05-06 18:36:01 +0530

    first commit

---
+++ app/Http/Controllers/SubAdminController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SubAdminController extends Controller
+{
+    public function dashboard(){
+        echo "I am Sub Admin";
+    }
+}
